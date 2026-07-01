9678fcd67e841a292df5990a4434d6ea25f3d3af
Will Coombs <w.m.coombs@durham.ac.uk>
2026-07-01 14:21:41 +0100

    Update Hooke3d.m

--- constitutive/Hooke3d.m
+++ constitutive/Hooke3d.m
@@ -1,16 +1,18 @@
-function [D,sig,epsE] = Hooke3d(epsE,mCst)
+function [D,sig,epsE] = Hooke3d(sigN,epsEn,epsE,mCst)
 %Linear elastic constitutive model
 %--------------------------------------------------------------------------
 % Author: William Coombs
-% Date:   29/01/2019
+% Date:   01/07/2026
 % Description:
 % Small strain linear elastic constitutive model 
 %
 %--------------------------------------------------------------------------
-% [D,sig,epsE] = HOOKE3D(epsE,mCst)
+% [D,sig,epsE] = HOOKE3D(sigN,epsEn,epsE,mCst)
 %--------------------------------------------------------------------------
 % Input(s):
-% epsE  - elastic strain (6,1)
+% sigN  - previos stress (6,1)
+% epsEn - previous elastic strain (6,1)
+% epsE  - current elastic strain (6,1)
 % mCst  - material constants 
 %--------------------------------------------------------------------------
 % Ouput(s);
@@ -31,5 +33,5 @@ bm11 = [1 1 1 0 0 0 ;
         0 0 0 0 0 0 ;
         0 0 0 0 0 0 ];
 D=E/((1+v)*(1-2*v))*(bm11*v+diag([1 1 1 0.5 0.5 0.5])*(1-2*v));             % elastic stiffness
-sig=D*epsE;                                                                 % stress 
+sig=sigN+D*(epsE-epsEn);                                                    % stress 
 end
